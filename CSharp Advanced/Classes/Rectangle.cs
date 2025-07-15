e830c1a68c77f78acb98ac720721b971ba821c4c
fox <textfox10000@gmail.com>
2025-07-16 00:28:12 +0300

    Problem 02

---
+++ CSharp Advanced/Classes/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Advanced.Classes
+{
+    internal class Rectangle
+    {
+        #region Q2) Create a class named Rectangle with the following constructors:
+        public int width { get; set; }
+        public int height { get; set; }
+
+        public Rectangle()
+        {
+            width = height = 0;
+        }
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public Rectangle(int WH)
+        {
+            height = width = WH;
+        }
+        public override string ToString()
+        {
+            return $"Width: {width}, Height: {height}";
+        }
+        #endregion
+    }
+}
--- CSharp Advanced/Program.cs
+++ CSharp Advanced/Program.cs
@@ -7,15 +7,27 @@ namespace CSharp_Advanced
         static void Main(string[] args)
         {
             #region Q1) Write a class named Calculator that contains a method named Add. Overload the Add method to:
-            int result1 = Calculator.Add(10, 60);
-            int result2 = Calculator.Add(10, 60, 20);
-            double result3 = Calculator.Add(10.6, 8.6);
+            //int result1 = Calculator.Add(10, 60);
+            //int result2 = Calculator.Add(10, 60, 20);
+            //double result3 = Calculator.Add(10.6, 8.6);
 
-            Console.WriteLine(result1);
-            Console.WriteLine(result2);
-            Console.WriteLine(result3);
+            //Console.WriteLine(result1);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(result3);
             #endregion
 
+            #region Q2) Create a class named Rectangle with the following constructors:
+            Rectangle r = new Rectangle();
+            r = new Rectangle();
+            Console.WriteLine(r);
+
+            r = new Rectangle(1,6);
+            Console.WriteLine(r);
+
+            r = new Rectangle(6);
+            Console.WriteLine(r);
+
+            #endregion
         }
     }
 }
