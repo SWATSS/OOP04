d67f1a3d1dd706d03af464bc3561d2e882ca3a8e
fox <textfox10000@gmail.com>
2025-07-16 00:19:40 +0300

    Problem 01

--- CSharp Advanced/Program.cs
+++ CSharp Advanced/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace CSharp_Advanced
+﻿using CSharp_Advanced.Classes;
+
+namespace CSharp_Advanced
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Q1) Write a class named Calculator that contains a method named Add. Overload the Add method to:
+            int result1 = Calculator.Add(10, 60);
+            int result2 = Calculator.Add(10, 60, 20);
+            double result3 = Calculator.Add(10.6, 8.6);
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            #endregion
+
         }
     }
 }
