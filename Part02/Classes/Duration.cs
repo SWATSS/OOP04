f9bb58ac7f90a4d2b33cc68972235294f3ed5ec8
fox <textfox10000@gmail.com>
2025-07-17 23:58:41 +0300

    Part 02

--- Part02/Classes/Duration.cs
+++ Part02/Classes/Duration.cs
@@ -41,6 +41,121 @@ namespace Part02.Classes
             }
         }
 
+        #region 4-Implement All required Operators overloading to enable this Code:
+        public static Duration operator +(Duration d1, Duration d2)
+        {
+            return new Duration
+            (
+                d1.Hours + d2.Hours,
+            d1.Minutes + d2.Minutes,
+            d1.Seconds + d2.Seconds
+            );
+        }
+        public static Duration operator -(Duration d1, Duration d2)
+        {
+            return new Duration
+            (
+                d1.Hours - d2.Hours,
+            d1.Minutes - d2.Minutes,
+            d1.Seconds - d2.Seconds
+            );
+        }
+        public static Duration operator +(Duration d1, int d2)
+        {
+            Duration d3 = new Duration(d2);
+
+            return new Duration
+            (
+                d1.Hours + d3.Hours,
+            d1.Minutes + d3.Minutes,
+            d1.Seconds + d3.Seconds
+            );
+        }
+        public static Duration operator +(int d1, Duration d2)
+        {
+            Duration d3 = new Duration(d1);
+
+            return new Duration
+            (
+                d2.Hours + d3.Hours,
+            d2.Minutes + d3.Minutes,
+            d2.Seconds + d3.Seconds
+            );
+        }
+        public static Duration operator ++(Duration d1)
+        {
+            int H = d1.Hours;
+            int M = d1.Minutes + 1;
+
+            if (M >= 60)
+                M = 0;
+                H++;
+            return new Duration(H, M, d1.Seconds);
+        }
+        public static Duration operator --(Duration d1)
+        {
+            int H = d1.Hours;
+            int M = d1.Minutes - 1;
+
+            if (M < 0)
+                M = 59;
+            H = H > 0 ? H - 1 : 0;
+            return new Duration(H, M, d1.Seconds);
+        }
+
+        public static bool operator >(Duration d1, Duration d2)
+        {
+            if (d1.Hours == d2.Hours)
+                if (d1.Minutes == d2.Minutes)
+                    return d1.Seconds > d2.Seconds;
+                else if (d1.Minutes == d2.Minutes)
+                    return d1.Seconds > d2.Seconds;
+            return d1.Hours > d2.Hours;
+        }
+        public static bool operator <(Duration d1, Duration d2)
+        {
+
+            if (d1.Hours == d2.Hours)
+                if (d1.Minutes == d2.Minutes)
+                    return d1.Seconds < d2.Seconds;
+                else if (d1.Minutes == d2.Minutes)
+                    return d1.Seconds < d2.Seconds;
+            return d1.Hours < d2.Hours;
+        }
+
+        public static bool operator >=(Duration d1, Duration d2)
+        {
+            int Time1 = (d1.Hours * 3600)+ (d1.Minutes * 60) + d1.Seconds;
+            int Time2 = (d2.Hours * 3600)+ (d2.Minutes * 60) + d2.Seconds;
+
+            return Time1 >= Time2;
+        }
+        public static bool operator <=(Duration d1, Duration d2)
+        {
+            int Time1 = (d1.Hours * 3600) + (d1.Minutes * 60) + d1.Seconds;
+            int Time2 = (d2.Hours * 3600) + (d2.Minutes * 60) + d2.Seconds;
+
+            return Time1 <= Time2;
+        }
+
+        public static bool operator true(Duration d1)
+        {
+            return d1.Hours !=0 || d1.Minutes != 0 || d1.Seconds != 0;
+        }
+        public static bool operator false(Duration d1)
+        {
+            return d1.Hours == 0 && d1.Minutes == 0 && d1.Seconds == 0;
+        }
+
+        public static explicit operator DateTime(Duration d1)
+        {
+            DateTime dateTime = DateTime.Today;// to Set Time (0, 0, 0)
+            return dateTime.AddHours(d1.Hours).AddMinutes(d1.Minutes).AddMinutes(d1.Seconds);
+            // I Searchd for bc if u used this (return new DateTime(2000, 1, 1, d.Hours, d.Minutes, d.Seconds);)
+            // I'm gonna need to Set the (D, M, Y) and it will be static
+        }
+
+        #endregion
 
         #endregion
         #region 2-Override All System. Object Members [To String(), Equals(),GetHashCode() ] .
--- Part02/Program.cs
+++ Part02/Program.cs
@@ -6,17 +6,37 @@ namespace Part02
     {
         static void Main(string[] args)
         {
+            #region 3 - Define All Required Constructors to Produce this output:
             Duration D1 = new Duration(1, 10, 15);
-            Console.WriteLine(D1);
+            //Console.WriteLine(D1);
 
             Duration D2 = new Duration(3600);
-            Console.WriteLine(D2);
+            //Console.WriteLine(D2);
 
             Duration D3 = new Duration(7800);
-            Console.WriteLine(D3);
+            //Console.WriteLine(D3);
 
             Duration D4 = new Duration(666);
-            Console.WriteLine(D4);
+            //Console.WriteLine(D4);
+            #endregion
+
+            #region 4-Implement All required Operators overloading to enable this Code:
+            D3 = D1 + D2;
+            Console.WriteLine(D3);
+            D3 = D1 + 7800;
+            Console.WriteLine(D3);
+            D3 = 666 + D3;
+            Console.WriteLine(D3);
+            D3 = ++D1;
+            D3 = --D2;
+            D1 = D1 - D2;
+            if (D1 > D2) ;
+            if (D1 <= D2) ;
+            if (D1) ;
+            DateTime Obj = (DateTime)D1;
+            Console.WriteLine(Obj);
+
+            #endregion
         }
     }
 }
